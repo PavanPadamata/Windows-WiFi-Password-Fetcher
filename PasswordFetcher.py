4b3d72dd3ba477b4cba103eedeba9f359df4c0c1
PavanPadamata <pavanpadmata2002@gmail.com>
2023-07-14 18:55:04 +0530

    [Fix] Update decoding codec for netsh command output. *This commit addresses an issue related to decoding the output of the netsh command using the UTF-8 codec. The previous implementation assumed that the output would always be encoded in UTF-8, which caused problems when encountering non-UTF-8 characters. *Updated the decoding codec to "latin-1" for decoding netsh command output.

--- PasswordFetcher.py
+++ PasswordFetcher.py
@@ -3,14 +3,14 @@ import subprocess
 def get_wifi_passwords():
     # Run netsh command to get Wi-Fi profiles and passwords
     command = "netsh wlan show profile key=clear"
-    output = subprocess.check_output(command, shell=True).decode("utf-8")
+    output = subprocess.check_output(command, shell=True).decode("latin-1")
     
     # Extract profile names and passwords
     profiles = [line.split(":")[1].strip() for line in output.split("\n") if "All User Profile" in line]
     passwords = []
     for profile in profiles:
         command = f"netsh wlan show profile name=\"{profile}\" key=clear"
-        profile_output = subprocess.check_output(command, shell=True).decode("utf-8")
+        profile_output = subprocess.check_output(command, shell=True).decode("latin-1")
         password_line = [line.split(":")[1].strip() for line in profile_output.split("\n") if "Key Content" in line]
         if password_line:
             passwords.append(password_line[0])
