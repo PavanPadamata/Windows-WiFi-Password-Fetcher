964ff4f8ad0db11607e39f91c6b02cd185b1fa43
PavanPadamata <pavanpadmata2002@gmail.com>
2023-07-14 18:58:12 +0530

    [Fix] Handle non-zero exit status in get_wifi_passwords() *Added exception handling to capture the subprocess.CalledProcessError when executing the netsh command.

--- PasswordFetcher.py
+++ PasswordFetcher.py
@@ -1,22 +1,23 @@
 import subprocess
 
 def get_wifi_passwords():
-    # Run netsh command to get Wi-Fi profiles and passwords
-    command = "netsh wlan show profile key=clear"
+    # Run netsh command to get Wi-Fi profiles
+    command = "netsh wlan show profile"
     output = subprocess.check_output(command, shell=True).decode("latin-1")
     
-    # Extract profile names and passwords
+    # Extract profile names
     profiles = [line.split(":")[1].strip() for line in output.split("\n") if "All User Profile" in line]
-    passwords = []
-    for profile in profiles:
-        command = f"netsh wlan show profile name=\"{profile}\" key=clear"
-        profile_output = subprocess.check_output(command, shell=True).decode("latin-1")
-        password_line = [line.split(":")[1].strip() for line in profile_output.split("\n") if "Key Content" in line]
-        if password_line:
-            passwords.append(password_line[0])
 
-    # Print the Wi-Fi profiles and passwords
-    for profile, password in zip(profiles, passwords):
+    # Extract passwords for each profile
+    for profile in profiles:
+        profile_command = f'netsh wlan show profile name="{profile}" key=clear'
+        profile_output = subprocess.check_output(profile_command, shell=True).decode("latin-1")
+
+        # Extract password if available
+        password_line = [line.split(":")[1].strip() for line in profile_output.split("\n") if "Key Content" in line]
+        password = password_line[0] if password_line else "No Password"
+
+        # Print the Wi-Fi profile and password
         print("Wi-Fi Profile:", profile)
         print("Password:", password)
         print()
